90c681e08ba6443341b4582cf3faa2bad5e70779
Alejandro Pérez <isaacale25@gmail.com>
2026-02-12 12:36:52 -0400

    Fix: logical error in authentication middleware

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -1,7 +1,7 @@
 const { response } = require("express");
 
 const isAuthenticated = (request, response, next) => {
-  if (request.session.user === undefined) {
+  if (!request.session || request.session.user) {
     return response
       .status(401)
       .json("You do not have access. Identify yourself please.");
