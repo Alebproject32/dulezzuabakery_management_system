9bd38dd305e856712a9559e73b06ce8de4504266
Alejandro Pérez <isaacale25@gmail.com>
2026-02-14 05:11:59 -0400

    Individual contribution added variable

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const bodyParser = require("body-parser");
 const connectDB = require("./db/connect");
 //const inventoryRoutes = require("./routes/inventory"); Last routes
