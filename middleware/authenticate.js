637a4534e476ab76608348249c8ced6aec2bd5b7
Alejandro Pérez <isaacale25@gmail.com>
2026-02-18 17:55:11 -0400

    Fix authenticate middleware

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -10,7 +10,6 @@ const isAuthenticated = (request, response, next) => {
     .status(401)
     .json({ message: "You do not have access. Identify yourself please." });
 };
-next();
 
 module.exports = {
   isAuthenticated,
